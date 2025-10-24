a4325767cb11b947e380d1b1ef8b1d4f57745739
Nael Nagi Doligez <nael.nagi--doligez@epfedu.fr>
2025-10-24 11:16:37 +0200

    TP1 exercice 4

---
+++ TP1_stats_NAGIDOLIGEZ/src/tp1_stats_nagidoligez/TP1_stats_NAGIDOLIGEZ.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_stats_nagidoligez;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author jacqu
+ */
+public class TP1_stats_NAGIDOLIGEZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int[] tableau = new int[6];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer un nombre :");
+        int m = sc.nextInt();
+
+        System.out.println("Valeurs du tableau :");
+        for (int i = 0; i < m; i++) {
+            Random generateurAleat = new Random();
+            int n = generateurAleat.nextInt(5);
+            tableau[n] +=1;
+        }
+        for (int i =0;i<6;i++){
+        System.out.println("Les probabilités d'avoir "+i+" est : "+(((1.0)*tableau[i])*(m*1.0)/100)+" %");
+        }
+    
+
+// TODO code application logic here
+    }
+
+}
