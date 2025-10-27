37cfef6cb43ef3d16008a92e09da0f5e01e4d825
Nael Nagi Doligez <nael.nagi--doligez@epfedu.fr>
2025-10-27 12:37:10 +0100

    TP1 exercice 4

--- TP1_stats_NAGIDOLIGEZ/src/tp1_stats_nagidoligez/TP1_stats_NAGIDOLIGEZ.java
+++ TP1_stats_NAGIDOLIGEZ/src/tp1_stats_nagidoligez/TP1_stats_NAGIDOLIGEZ.java
@@ -27,15 +27,14 @@ public class TP1_stats_NAGIDOLIGEZ {
         System.out.println("Valeurs du tableau :");
         for (int i = 0; i < m; i++) {
             Random generateurAleat = new Random();
-            int n = generateurAleat.nextInt(5);
-            tableau[n] +=1;
+            int n = generateurAleat.nextInt(6);
+            tableau[n] += 1;
         }
-        for (int i =0;i<6;i++){
-        System.out.println("Les probabilités d'avoir "+i+" est : "+(((1.0)*tableau[i])*(m*1.0)/100)+" %");
+        for (int i = 0; i < 6; i++) {
+            double proba = (tableau[i] * 100.0) / m;
+            System.out.println("Face " + (i + 1) + " : " + tableau[i] + " --> " + proba + "%");
         }
-    
 
 // TODO code application logic here
     }
-
 }
