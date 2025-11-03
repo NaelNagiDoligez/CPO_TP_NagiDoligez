53e65083526a72ae752e357ead6917091f7e372e
Nael Nagi Doligez <nael.nagi--doligez@epfedu.fr>
2025-11-03 15:30:14 +0100

    TP2 exercice 2

---
+++ TP2_convertisseurObjet_NAGIDOLIGEZ/src/tp2_convertisseurobjet_nagidoligez/Convertisseur.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_nagidoligez;
+
+/**
+ *
+ * @author jacqu
+ */
+public class Convertisseur {
+
+    int nbConversions;
+
+    public Convertisseur() {
+        nbConversions = 0;
+    }
+
+    public double CelciusVersKelvin(double tCelcius) {
+        tCelcius = tCelcius + 273.15;
+        return tCelcius;
+    }
+
+    public double KelvinVersCelcius(double tKelvin) {
+        tKelvin = tKelvin - 273.15;
+        return tKelvin;
+    }
+
+    public double FarenheitVersCelcius(double tFarenheit) {
+        tFarenheit = (tFarenheit - 32) * 5 / 9;
+        return tFarenheit;
+    }
+
+    public double CelciusVersFarenheit(double tCelcius) {
+        tCelcius = tCelcius * 1.8 + 32;
+        return tCelcius;
+    }
+
+    public double KelvinVersFarenheit(double tKelvin) {
+        tKelvin = (tKelvin - 273.15) * 1.8 + 32;
+        return tKelvin;
+    }
+
+    public double FarenheitVersKelvin(double tFarenheit) {
+        tFarenheit = (tFarenheit - 32) * 5 / 9 + 273.15;
+        return tFarenheit;
+    }
+
+    @Override
+    public String toString() {
+        return "nb de conversions" + nbConversions;
+    }
+    
+    
+}
