062ec7193f37195388a529213eb6316390512b92
Nael Nagi Doligez <nael.nagi--doligez@epfedu.fr>
2025-11-04 19:00:36 +0100

    TP2 exercice 3

---
+++ TP2_relation_1_NAGIDOLIGEZ/src/tp2_relation_1_nagidoligez/Personne.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_nagidoligez;
+
+/**
+ *
+ * @author jacqu
+ */
+public class Personne {
+    String prenom;
+    String nom;
+    
+}
