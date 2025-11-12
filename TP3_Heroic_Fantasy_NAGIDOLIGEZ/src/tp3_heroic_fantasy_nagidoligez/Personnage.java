b51af7e5e5bbcf7f255f359d72aa07fcbd9dff53
Nael Nagi Doligez <nael.nagi--doligez@epfedu.fr>
2025-11-12 13:00:51 +0100

    TP3

---
+++ TP3_Heroic_Fantasy_NAGIDOLIGEZ/src/tp3_heroic_fantasy_nagidoligez/Personnage.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_nagidoligez;
+
+/**
+ *
+ * @author jacqu
+ */
+public abstract class Personnage {
+    String nom;
+    int hp;
+
+    public Personnage(String nom, int hp) {
+        this.nom = nom;
+        this.hp = hp;
+    }
+    
+    public String nom(){
+        return nom;
+    }
+    
+    public
+
+    @Override
+    public String toString() {
+        return "Personnage{" + "nom=" + nom + ", hp=" + hp + '}';
+    }
+    
+    
+}
